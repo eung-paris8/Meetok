91bfab7c0d6f6a1c5991a7e8ebd402e030b99f8e
ilyes-annou <ilyesannou77@gmail.com>
2021-01-19 16:06:45 +0100

    19/01 profil

---
+++ modules/mod_Profil/profil_modele.php
@@ -0,0 +1,86 @@
+<?php
+	if (!defined('CONST_INCLUDE')){
+    	die('Accès direct interdit');
+	}
+
+
+	require_once('./include/connexion.php');
+
+	class ProfilModele extends Connexion{
+
+		private $idUser;
+		
+		function __construct($user){
+			$this->idUser = $user;
+			self::initConnexion();
+			//$this->idUser = 3;
+
+		}
+
+		/*function verif_profil($user){
+			$this->idUser = $user;
+		}*/
+
+		function getInfo(){
+			//$sql="SELECT * From utilisateur where id_Utilisateur=(?)";
+			$sql="SELECT id_Utilisateur ,nom_Utilisateur ,prenom_Utilisateur, age ,localisation ,sexe ,nom_Centres_d_Interets, description, photo FROM utilisateur NATURAL JOIN aime NATURAL JOIN centres_d_interets WHERE id_Utilisateur=(?);";
+			$result=self::$bdd->prepare($sql);
+			$result->execute(array($this->idUser));
+			$data=$result->fetch();
+			if($data['id_Utilisateur']==null){
+				die('Utilisateur inexistant');
+			}
+			//$data=$result->fetchAll();
+			return $data;
+		}
+
+		function getCentresInteret(){
+			$sql="SELECT nom_Centres_d_Interets FROM utilisateur NATURAL JOIN aime NATURAL JOIN centres_d_interets WHERE id_Utilisateur=(?);";
+			$result=self::$bdd->prepare($sql);
+			$result->execute(array($this->idUser));
+			$data=$result->fetchAll();
+			return $data;
+
+		}
+
+		function modif(){
+
+			
+
+			if ($_POST['localisation']!="" && $_POST['description']!="") {
+				$sql="UPDATE utilisateur SET localisation = (?), description = (?) where id_Utilisateur=(?)";
+				$result=self::$bdd->prepare($sql);
+				$result->execute(array(htmlspecialchars($_POST['localisation']), htmlspecialchars($_POST['description']), $this->idUser));
+			}
+
+			elseif ($_POST['localisation']=="") {
+				$sql="UPDATE utilisateur SET description = (?) where id_Utilisateur=(?)";
+				$result=self::$bdd->prepare($sql);
+				$result->execute(array(htmlspecialchars($_POST['description']), $this->idUser));
+			}
+
+			elseif ($_POST['description']=="") {
+				$sql="UPDATE utilisateur SET localisation = (?) where id_Utilisateur=(?)";
+				$result=self::$bdd->prepare($sql);
+				$result->execute(array(htmlspecialchars($_POST['localisation']), $this->idUser));
+			}
+
+			elseif ($_POST['localisation']=="" && $_POST['description']=="") {
+				# code...
+			}
+			
+		}
+
+		function report(){
+
+			$sql="INSERT into signalement values(null, (?), (?), (?), (?))";
+			$result=self::$bdd->prepare($sql);
+			$result->execute(array($_SESSION['id_Utilisateur'], $this->idUser, htmlspecialchars($_POST['titre']), htmlspecialchars($_POST['message'])));
+			$sql="INSERT into creer_signalement values((?), null)";
+			$result=self::$bdd->prepare($sql);
+			$result->execute(array($this->idUser));
+
+		}
+	}
+
+?>
---
+++ modules/mod_Profil/profil_vue.php
@@ -0,0 +1,239 @@
+<?php 
+    if (!defined('CONST_INCLUDE')){
+        die('Accès direct interdit');
+    }
+
+
+	class ProfilVue{
+
+		private $info;
+        private $ci;
+			
+		function __construct(){
+			
+		}
+		
+
+		function recoitInfo($data){
+			$this->info=$data;
+		}
+
+        function recoitCentresInteret($data){
+            $this->ci=$data;
+        }
+
+		function afficherVue(){
+			?>
+
+			<!DOCTYPE html>
+			<html>
+			<head>
+				<title>Profil</title>
+
+				<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+				<link rel="stylesheet" type="text/css" href="style.css">
+
+				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+				<script type="text/javascript">
+			
+					function deconnexion() {
+						if (window.confirm('Voulez-vous vous déconnecter?')) {
+  							window.location.href="index.php?module=Connexion&action=deconnexion";
+
+						} 
+					}
+					
+					$(document).ready(function(){
+
+						$(".slideform").hide();
+
+						$("#slidebutton").click(function(){
+    						$(".slideform").slideDown();
+  						});
+  						$("#cancel").click(function(){
+    						$(".slideform").slideUp();
+ 						});
+ 						
+					});
+					
+				</script>
+			</head>
+
+
+<!-- Section principale -->
+
+<div class="container_fluid sectionPrincipale">
+    <div class="container-xxl">
+        <div class="containerProfil mx-auto">
+            <div class="profilBox mx-auto">
+                <div class="row gx-0">
+                    <div class="sectionInformationsPerso">
+                        <div class="row gx-0">
+                            <div class="imgProfil col-xxl-5 col-xl-6 col-lg-8 me-auto">
+                                <img class="mx-auto my-auto d-block" alt="photo de profil" src=<?php
+                                if ($this->info["photo"]==NULL || $this->info["photo"]=="") {
+                                	echo "https://journalmetro.com/wp-content/uploads/2017/04/default_profile_400x400.png?w=860";
+                                }
+                                else{
+                                	echo($this->info["photo"]);
+                                }
+                                ?> >
+                            </div>
+                            <div class="infoPerso col-xxl-18 col-xl-17 col-lg-15 ms-auto my-auto">
+                                <p class="nom"> Nom: <?php
+                                    echo $this->info['nom_Utilisateur'];	
+								?> </p>
+                                <p class="prenom"> Prénom: <?php							
+									echo $this->info['prenom_Utilisateur'];		
+								?> </p>
+                                <p class="age"> Age: <?php							
+									echo $this->info['age'];		
+								?> </p>
+                                <p class="localisation"> Localisation: <?php							
+									echo $this->info['localisation'];		
+								?> </p>
+                                <p class="sexe"> Sexe: <?php							
+									echo $this->info['sexe'];		
+								?> </p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="sectionDescription mt-2">
+                        <p class="titre">Description:</p>
+                        <div class="containerDesc">
+                            <p>
+                                <?php							
+									echo $this->info['description'];		
+								?> 
+                            </p>
+                        </div>
+                    </div>
+                    <div class="sectionCentreInteret mt-2">
+                        <p class="titre">Centres d'intérêt:</p>
+                            <div class="CI">
+                                <div class="row row-cols-xxl-6 row-cols-xl-6 row-cols-lg-6 row-cols-md-3 row-cols-sm-2">
+                                    <div class="CI_1">
+                                        <p><?php
+                                        foreach ($this->ci as $ci) {
+                                            echo $ci['nom_Centres_d_Interets'].'<br>'; 
+                                        }
+                                         
+                                         ?></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+                    <?php  
+
+                        if (isset($_SESSION['id_Utilisateur']) && $_SESSION['id_Utilisateur']==$this->info['id_Utilisateur']) {
+                            
+                    ?>
+
+                    <button class='btn btn-primary' id="slidebutton">
+						Modifier
+					</button>
+
+					<div class="slideform">
+						<form method='POST' action="index.php?module=Profil&user=<?php echo($this->info['id_Utilisateur']) ?>&action=modif">
+                            <div class="form-group">
+                                <label>Localisation</label>
+                                <input type='text' class="form-control" name='localisation'>
+                            </div>
+							
+							<div class="form-group">
+                                <label>Description</label>
+                                <input type='text' class="form-control" name='description' >                       
+                            </div>
+
+                            
+
+
+							<input class='btn btn-primary' type='submit' name='envoi' value='Envoyer'>
+
+						</form>
+
+                        <br>
+
+                        <button class='btn btn-primary' id="cancel">
+                            Annuler
+                        </button>
+
+                    </div>
+
+                    <?php 
+
+                        }
+
+                        else{
+
+                    ?>
+
+                    <button class='btn btn-primary' id="slidebutton">
+                        Signaler
+                    </button>
+
+                    <div class="slideform">
+
+                        <form method="POST" action="index.php?module=Profil&user=<?php echo($this->info['id_Utilisateur']) ?>&action=report">
+
+                            <div class="form-group">
+                                <label>Raison</label>
+                                <input type='text' class="form-control" name='titre' required>
+                            </div>
+                            <div class="form-group">
+                                <label>Message (facultatif)</label>
+                                <input type='text' class="form-control" name='message'>
+                            </div>
+
+                            <input class='btn btn-primary' type='submit' name='envoi' value='Envoyer'> 
+                               
+
+                        </form>
+
+                        <br>
+
+                        <button class='btn btn-primary' id="cancel">
+                            Annuler
+                        </button>
+
+                    </div>   
+
+                        <?php
+                            
+                        }
+
+                        ?>
+
+
+						
+					
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Footer -->
+
+
+
+<!-- JavaScript Bootstrap -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+	</body>
+</html>
+
+			<?php
+		}
+		
+	}
+
+?>
+
